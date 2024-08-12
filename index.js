8b09cb612b031c3be2c16dd5deef6f3e74569363
lucasgabrl <165048979+lucasgabrl@users.noreply.github.com>
2024-08-12 23:10:22 +0000

    12/08/24

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const idade = 18;
+let idade = 18;
 const anoNascimento = 2005;
 let anoAtual = 2024;
+const nomeCompleto = "Lucas Gabriel";
+
+console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos");
 
 anoAtual = anoAtual + 1;
 
 console.log(anoAtual);
+
+idade = idade + 1;
+console.log(idade);
